2c5bb1f2c0e249939aa94538daebccdb6c30758e
gyloh <ronaldo_loh@hotmail.com>
2024-10-06 18:36:15 +0800

    init commit for react app

---
+++ react-app/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; // Theme
+import 'primereact/resources/primereact.min.css';                 // Core CSS
+import 'primeicons/primeicons.css';                               // Icons
+import 'primeflex/primeflex.css';                                 // Flex CSS
+import './index.css';                                             // Custom CSS
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
