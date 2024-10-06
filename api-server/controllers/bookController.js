fd2135e7d6ff733b455b338901bacb635aa53371
gyloh <ronaldo_loh@hotmail.com>
2024-10-06 17:22:11 +0800

    add GET books API

--- api-server/controllers/bookController.js
+++ api-server/controllers/bookController.js
@@ -3,6 +3,24 @@ const validate = require("../middleware/validate");
 const { createBookSchema } = require("../schema/book");
 const router = express.Router();
 
+const books = [
+  {
+    id: 1,
+    title: "Lord of the Ring",
+    description: "One of the best selling in 2020",
+  },
+  {
+    id: 2,
+    title: "Harry Potter",
+    description: "Most recommended for teenages",
+  },
+  {
+    id: 3,
+    title: "Naruto",
+    description: "One of the trending manga series now",
+  },
+];
+
 router.post("/", validate(createBookSchema), async function (req, res) {
   const { title, description } = req.body;
 
@@ -13,6 +31,12 @@ router.post("/", validate(createBookSchema), async function (req, res) {
 
   return res.json({
     message: "Created successfully",
+  });
+});
+
+router.get("/", async function (req, res) {
+  return res.json({
+    books,
   });
 });
 
